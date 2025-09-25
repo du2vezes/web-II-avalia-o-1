a505c9d98f3ecf813f571c775fe5fa79fcc1004f
du2vezes <duduwinss@gmail.com>
2025-09-24 23:22:32 -0300

    Adiciona visualização Redoc e corrige nome da rota Swagger na configuração de URLs

--- JobConnect/JobConnect/urls.py
+++ JobConnect/JobConnect/urls.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -14,8 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include(api_urls)),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="docschema"), name="swagger-ui"),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    
-]
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),]
