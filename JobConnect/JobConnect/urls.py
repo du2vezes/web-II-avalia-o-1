08a8880a984deb0e1ac69e8d1afdc35212ea3d4b
du2vezes <duduwinss@gmail.com>
2025-09-25 15:08:02 -0300

    bom e novo

--- JobConnect/JobConnect/urls.py
+++ JobConnect/JobConnect/urls.py
@@ -13,8 +13,8 @@ from vagaEmprego.api.v1.router import api_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include(api_urls)),
-    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/v1/docs/", SpectacularSwaggerView.as_view(url_name="docschema"), name="swagger-ui"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="docschema"), name="swagger-ui"),
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),]
--- JobConnect/vagaEmprego/api/v1/viewsets.py
+++ JobConnect/vagaEmprego/api/v1/viewsets.py
@@ -1,15 +1,19 @@
 from rest_framework import viewsets
 from vagaEmprego.api.v1 import serializers
 from vagaEmprego import models
+from rest_framework.permissions import IsAuthenticated
 
 class EmpresaViewSet(viewsets.ModelViewSet):
     queryset = models.Empresa.objects.filter(is_deleted=False)
     serializer_class = serializers.EmpresaSerializer
+    permission_classes = [IsAuthenticated]
 
 class VagaViewSet(viewsets.ModelViewSet):
     queryset = models.Vaga.objects.filter(is_deleted=False)
     serializer_class = serializers.VagaSerializer
+    permission_classes = [IsAuthenticated]
 
 class CandidatoViewSet(viewsets.ModelViewSet):
     queryset = models.Candidato.objects.filter(is_deleted=False)
     serializer_class = serializers.CandidatoSerializer
+    permission_classes = [IsAuthenticated]  
